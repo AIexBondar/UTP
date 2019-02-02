da3e635d35f16e65a679493d1551e48e95ae0177
Robert <robertw3256@gmail.com>
2019-02-02 19:52:05 +0100

    UTP12

---
+++ UTP12/src/zad11/Database.java
@@ -0,0 +1,92 @@
+package zad11;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Database {
+	private Connection connection;
+	private TravelData travelData;
+	private static String driverName = "org.apache.derby.jdbc.EmbeddedDriver";
+	private String url;
+	private Integer id = 1;
+	
+	public Database(String url, TravelData travelData) {
+		this.url = url;
+		this.travelData = travelData;
+	}
+
+	public void create() {
+		try {
+			Class.forName(driverName);
+			connection = DriverManager.getConnection(url);
+			if(connection != null) {
+				System.out.println();
+				System.out.println("Connected to derby!");
+			}
+		} catch (SQLException e) {
+			System.err.println("Connection with DB exe");
+			System.exit(2);
+		} catch (ClassNotFoundException e) {
+			System.err.println("Driver Class not found");
+			e.printStackTrace();
+			System.exit(2);
+		}
+		
+		try {
+			connection.createStatement().execute("CREATE TABLE Oferta("
+					+ "id int PRIMARY KEY, "
+					+ "kraj varchar(40), "
+					+ "data_wyjazdu Date, "
+					+ "data_powrotu Date, "
+					+ "miejsce varchar(20), "
+					+ "cena varchar(20), "
+					+ "symbol_waluty varchar(10))"
+					);
+			
+			for (String line : travelData.getResList()) {
+				String[] tokens = line.split("\\t");
+				connection.createStatement().execute(
+						"INSERT INTO Oferta(id, kraj, data_wyjazdu, data_powrotu, miejsce, cena, symbol_waluty) VALUES "
+						+ "("+id+", "+"'"+tokens[0]+"', " + "'"+tokens[1]+"', " + "'"+tokens[2]+"', " + "'"+tokens[3]+"'," + "'"+tokens[4]+"', " + "'"+tokens[5]+"')"
+						);
+				
+				id++;
+			}
+			
+		} catch (SQLException e) {
+			System.err.println("DDL&DML exe");
+			e.printStackTrace();
+		}
+	}
+
+	public void showGui() {
+		Statement statment;
+		
+		try {
+			statment = connection.createStatement();
+			ResultSet resSet = statment.executeQuery("SELECT * FROM Oferta");
+			ResultSetMetaData resMeta = resSet.getMetaData();
+			int columnCount = resMeta.getColumnCount();
+			
+			for (int i = 1; i <= columnCount; i++)
+				System.out.format("%20s", resMeta.getColumnName(i) + " | ");
+			
+			while(resSet.next()) {
+				System.out.println("");
+				for (int i = 1; i <= columnCount; i++)
+					System.out.format("%20s", resSet.getString(i) + " | ");
+			}
+			
+			statment.close();
+			connection.close();
+		} catch (SQLException e) {
+			System.err.println("Show data exe");
+			e.printStackTrace();
+			System.exit(2);
+		}	
+	}
+}
---
+++ UTP12/src/zad11/TravelData.java
@@ -0,0 +1,117 @@
+package zad11;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class TravelData {
+	private ArrayList<ArrayList<String>> dataList = new ArrayList<ArrayList<String>>();
+	private ArrayList<String> resList = null;
+	private File file; 
+	private Scanner sc;
+	private Locale localeSaved;
+	
+	public TravelData(File dataDir) {
+		try {
+			file = new File(dataDir + "\\dane");
+			sc = new Scanner(file);
+		} catch (FileNotFoundException e) {
+			System.err.println("File read exe");
+		}
+		
+		while (sc.hasNextLine()){ 
+			String line = sc.nextLine();
+			String[] tokens = line.split("\\t");
+			ArrayList<String> tmpList = new ArrayList<String>();
+			for (int i = 0; i < tokens.length; i++)
+				tmpList.add(tokens[i]);
+			
+			dataList.add(tmpList);
+		} 
+    }
+	
+	public List<String> getOffersDescriptionsList(String localeString, String dateFormat) {
+		resList = new ArrayList<String>();
+		String[] tokens = localeString.split("_");
+		String resString = new String();
+		
+		if(tokens.length > 1) {
+			localeSaved = new Locale(tokens[0], tokens[1]);
+		}else {
+			localeSaved = new Locale(tokens[0]);
+		}
+		
+		SimpleDateFormat simpleDateFormat = (SimpleDateFormat) DateFormat.getDateInstance();
+		simpleDateFormat.applyPattern(dateFormat);
+		
+		Date data = null;
+		for (ArrayList<String> list : dataList) {
+			String[] tokensTmp = list.get(0).split("_");
+			Locale locale = null;
+
+			if(tokensTmp.length > 1) {
+				locale = new Locale(tokensTmp[0], tokensTmp[1]);
+			}else {
+				locale = new Locale(tokensTmp[0]);
+			}
+			
+			Locale.setDefault(locale);
+			
+			//kraj
+			Locale[] locales = Locale.getAvailableLocales();
+			Locale localeTranslate = null;
+			
+			for (int i = 0; i < locales.length; i++) {
+				if(locales[i].getDisplayCountry().equals(list.get(1)))
+					localeTranslate = locales[i];
+			}
+			resString += localeTranslate.getDisplayCountry(localeSaved);
+			
+			try {
+				//date_wyjazdu
+				data = simpleDateFormat.parse(list.get(2));
+				resString += "	" + simpleDateFormat.format(data);
+				
+				//date_powrotu 
+				data = simpleDateFormat.parse(list.get(3));
+				resString += "	" + simpleDateFormat.format(data);
+				
+				//miejsce
+				//ResourceBundle bundle = ResourceBundle.getBundle("PlacesInfo", localeSaved);
+				//resString += " " + bundle.getString(list.get(4));
+				resString += "	" + list.get(4);
+				
+				//cena
+				NumberFormat nFormat = NumberFormat.getInstance(locale);
+				Number number = nFormat.parse(list.get(5));
+				resString += "	" + nFormat.format(number);
+				
+				//waluta
+				Currency currency = Currency.getInstance(list.get(6));
+				resString += "	" + currency.getCurrencyCode();
+				
+				resList.add(resString);
+				resString = new String();
+				
+				} catch (ParseException e) {
+				System.err.println("Format exe");
+				System.exit(2);
+			}
+		}
+		return resList;
+	}
+	
+	public ArrayList<String> getResList() {
+		return resList;
+	}
+}
