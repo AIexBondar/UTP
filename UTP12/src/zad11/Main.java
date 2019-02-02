da0f91212a24b87e952cdc95d145d76f8e78f389
Robert <robertw3256@gmail.com>
2019-02-02 19:57:11 +0100

    .

--- UTP12/src/zad11/Main.java
+++ UTP12/src/zad11/Main.java
@@ -20,7 +20,7 @@ public class Main {
       List<String> odlist = travelData.getOffersDescriptionsList(locale, dateFormat);
       for (String od : odlist) System.out.println(od);
     }
-    // --- część bazodanowa
+    
     String url = "jdbc:derby:myDataBaseDerby;create=true";
     Database db = new Database(url, travelData);
     db.create();
